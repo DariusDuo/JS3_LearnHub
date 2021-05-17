a5135b64ff6c14acf52ceb8636471696deab4deb
Darius Duoba <darius.duoba@gmail.com>
2021-05-17 19:13:40 +0300

    minor changes before time travel

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 "use strict";
 const container = document.querySelector(".container");
-const dateLine = document.querySelector("p");
+const dateLine = document.createElement("h4");
 container.appendChild(dateLine);
 dateLine.textContent = "here goes date";
 
@@ -11,3 +11,10 @@ let cYear = currentDate.getFullYear();
 let time = currentDate.getHours() + ":" + currentDate.getMinutes() + ":" + currentDate.getSeconds();
 
 dateLine.textContent = `${cYear}-${cMonth}-${cDay} ${time}`;
+
+const timeEl = document.getElementById("time");
+function clock() {
+  setInterval(() => (timeEl.textContent = getTime()), 1000);
+}
+
+clock();
