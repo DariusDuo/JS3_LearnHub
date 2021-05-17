4d3a7bd2933f7bfb6def4a52f3f2d22e2d821a1b
Darius Duoba <darius.duoba@gmail.com>
2021-05-17 15:32:11 +0300

    date and time added

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 "use strict";
+const container = document.querySelector(".container");
+const dateLine = document.querySelector("p");
+container.appendChild(dateLine);
+dateLine.textContent = "here goes date";
+
+let currentDate = new Date();
+let cDay = currentDate.getDate();
+let cMonth = currentDate.getMonth() + 1;
+let cYear = currentDate.getFullYear();
+let time = currentDate.getHours() + ":" + currentDate.getMinutes() + ":" + currentDate.getSeconds();
+
+dateLine.textContent = `${cYear}-${cMonth}-${cDay} ${time}`;
