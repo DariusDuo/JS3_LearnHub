f7beb9a635e6b7c632a86804678652dea5d130be
Darius Duoba <darius.duoba@gmail.com>
2021-05-18 10:07:25 +0300

    test

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 "use strict";
 const container = document.querySelector(".container");
 const dateLine = document.createElement("h4");
+const copyrightEl = document.createElement("h5");
+const footer = document.querySelector("footer");
+
+dateLine.id = "time";
 container.appendChild(dateLine);
 dateLine.textContent = "here goes date";
 
@@ -18,3 +22,10 @@ function clock() {
 }
 
 clock();
+
+copyrightEl.textContent = "Copyright blablabla";
+footer.appendChild(copyrightEl);
+const header = document.createElement("header");
+header.id = "simpleHeader";
+const body = document.querySelector("body");
+body.insertAdjacentElement("afterbegin", header);
